736897cb8e11b8ffd0e079401002345fd9380cf8
AmirrezaAP <AmirrezaAPTech@gmail.com>
2024-06-22 05:03:29 +0330

    Navbar

---
+++ front-end/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+        <div className='w-screen'>
+          <Header />
+        </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
+
+
+const Header = () => {
+  return (
+    <div className="sticky flex justify-between items-center w-[90%] mx-auto mt-5">
+        <div className="flex justify-center items-center gap-1">
+        <div className="bg-white w-[20px] h-[20px] rounded-full"></div>
+        <h1 className="font-semibold text-3xl">TrackMate</h1>
+        </div>
+        <div className="flex justify-center items-center gap-5 text-white text-xl">
+            <a href=""><h2 className="text-2xl font-normal">FAQ</h2></a>
+            <a href="" className='flex justify-center items-center gap-1'><FontAwesomeIcon icon={faCircleUser} /> <p>Log In</p></a>
+            <button className='bg-[#A238FF] text-white font-normal text-md px-6 py-1 rounded-3xl shadow-md shadow-[#a238ff7d] '>Buy Now</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
