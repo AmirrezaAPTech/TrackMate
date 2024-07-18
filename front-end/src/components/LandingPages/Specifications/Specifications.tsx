6d55161adcd79a2c0427a5100cd7bf9775b054dd
AmirrezaAP <AmirrezaAPTech@gmail.com>
2024-07-18 18:47:48 +0330

    Specifications Responsive

--- front-end/src/components/LandingPages/Specifications/Specifications.tsx
+++ front-end/src/components/LandingPages/Specifications/Specifications.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
 import ProductImage from "../../../Img/Specifications_ProductImg.webp"
 import BatteryIcon from "../../../Img/BatteryIcon.webp"
+import Bluetooth from "../../../Img/BluetoothIcon.webp"
+import WaterIcon from "../../../Img/WaterIcon.webp"
+import WirelessIcon from "../../../Img/WirelessIcon.webp"
 import SpecificationsCard from './SpecificationsCard';
+import ButtonDecoration from '../../ButtonDecoration';
 
 const Specifications = () => {
     const [scrollOpacity, setScrollOpacity] = useState(0);
@@ -27,32 +31,34 @@ const Specifications = () => {
     }, []);
   return (
     <div>
-        <div className="h-44 flex justify-center items-end z-50">
-        <h2 className="text-6xl text-white">0.4 TrackMate XE Specifications</h2>
+        <div className="h-44 flex justify-center items-end  z-50">
+        <h2 className="text-6xl min-[2260px]:text-8xl 2xl:text-6xl max-xl:text-5xl max-sm:text-center max-md:text-4xl max-sm:text-5xl max-[500px]:text-4xl max-sm:font-semibold">0.4 TrackMate XE Specifications</h2>
         </div>
     <div id="specifications" className="specifications-image-container h-auto w-screen flex flex-col justify-center gap-y-20 items-center py-32 z-50 relative">
-    <div className="absolute inset-0 bg-gradient-to-b from-[#232323] via-[#232323]/20 to-transparent w-full h-full"style={{ backgroundColor: `rgba(34, 35, 35, ${scrollOpacity})` }}></div>
-        <img src={ProductImage} alt="ProductImage" className="w-[678px] h-[871px] z-10"/>
-    <div className='absolute z-50 flex flex-col justify-between items-center h-[871px]'>
-        <div className='flex justify-between items-center w-[1200px]'>
-            <div className='absolute top-24 left-0'>
+    <div className="absolute inset-0 bg-gradient-to-b from-[#232323] via-[#232323]/20 to-transparent w-full h-auto"style={{ backgroundColor: `rgba(34, 35, 35, ${scrollOpacity})` }}></div>
+        <img src={ProductImage} alt="ProductImage" className="lg:w-[678px] lg:h-[871px] sm:w-[600px] sm:h-[793px] z-10"/>
+    <div className='xl:absolute z-50 flex flex-col justify-center max-xl:w-full xl:justify-between items-center h-[871px]  gap-y-8 '>
+        <div className='flex flex-col gap-y-8 xl:flex-row justify-between items-center xl:w-[1200px]'>
+            <div className='xl:absolute top-24 left-0'>
         <SpecificationsCard imgUrl={BatteryIcon} title='20 Hour Battery Life' />
             </div>
-            <div className='absolute top-0 right-0'>
-        <SpecificationsCard imgUrl={BatteryIcon} title='20 Hour Battery Life' />
+            <div className='xl:absolute top-0 right-0'>
+        <SpecificationsCard imgUrl={Bluetooth} title='Bluetooth Connectivity' />
             </div>
         </div>
-        <div className='flex justify-between items-center w-[1200px]'>
-        <div className='absolute bottom-0 left-0'>
-        <SpecificationsCard imgUrl={BatteryIcon} title='20 Hour Battery Life' />
+        <div className='flex flex-col  gap-y-8  xl:flex-row  justify-between items-center xl:w-[1200px]'>
+        <div className='xl:absolute bottom-0 left-0'>
+        <SpecificationsCard imgUrl={WirelessIcon} title='Wireless Charging' />
             </div>
-            <div className='absolute bottom-24 right-0'>
-        <SpecificationsCard imgUrl={BatteryIcon} title='20 Hour Battery Life' />
+            <div className='xl:absolute bottom-24 right-0'>
+        <SpecificationsCard imgUrl={WaterIcon} title='Water Resistance' />
             </div>
         </div>
     </div>
-    <button className='bg-[#A238FF] text-white font-normal text-lg px-20 py-2 mt-12 rounded-3xl shadow-custom z-50 transition duration-300 hover:text-black hover:bg-white'>Buy Now</button>
+    <div className='z-50'>
+    <ButtonDecoration customCLass='font-normal text-lg px-20 py-2 mt-12'/>
     </div>
+        </div>
     </div>
   )
 }
--- front-end/src/components/LandingPages/Specifications/SpecificationsCard.tsx
+++ front-end/src/components/LandingPages/Specifications/SpecificationsCard.tsx
@@ -7,9 +7,9 @@ interface SpecificationsCardProps {
 
 const SpecificationsCard: React.FC<SpecificationsCardProps> = ({ imgUrl, title }) => {
   return (
-    <div className="w-[400px] h-[170.38px] bg-[#222323] rounded-2xl flex flex-col items-start justify-center p-4 gap-4">
-        <div className='flex justify-start items-center gap-3'>
-      <img src={imgUrl} alt={title} className="w-16 h-16 object-cover rounded-full" />
+    <div className="w-[280px] sm:w-[400px] sm:h-[170.38px]  bg-[#222323] rounded-2xl flex flex-col items-start justify-center p-4 gap-4 shadow-2xl shadow-black max-sm:text-center">
+        <div className='flex w-full flex-col sm:flex-row sm:justify-start justify-center items-center gap-3'>
+      <img src={imgUrl} alt={title} className="sm:w-16 w-12 h-12 sm:h-16 object-cover rounded-full" />
       <h3 className="text-white text-xl font-semibold">{title}</h3>
         </div>
         <p>I'm a paragraph. Click here to add your own text and edit me.</p>
