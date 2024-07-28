7274d74a639b6ea5c6d9c6890d3ea933891dbb66
AmirrezaAP <AmirrezaAPTech@gmail.com>
2024-07-28 05:19:17 +0330

    Add to cart and Selected Color

--- front-end/src/components/ProductPage/ProductPage.tsx
+++ front-end/src/components/ProductPage/ProductPage.tsx
@@ -1,15 +1,24 @@
-// import ProductImages from "./ProductImages"
 import { useState } from "react";
-import ProductWhite from "../../Img/ProductImages/ProductWhite.webp"
 import CollapsibleSection from "./CollapsibleSection";
-
+import TrackMateColors from '../../constants/constants'
 
 const ProductPage = () => {
   const [openSection, setOpenSection] = useState<string | null>(null);
+  const [selectedColor, setSelectedColor] = useState<string>("white");
+  const [productQuantity, setProductQuantity] = useState<number>(1);
 
   const handleToggle = (section: string) => {
     setOpenSection(openSection === section ? null : section);
   };
+  
+    const selectedColorData = TrackMateColors.find(TrackMateColor => TrackMateColor.color === selectedColor);
+    const selectedColorImageUrl = selectedColorData ? selectedColorData.imageUrl : TrackMateColors[0].imageUrl;
+
+  const addToCart = () => {
+    selectedColorData!.quantity = productQuantity + selectedColorData!.quantity;
+    setProductQuantity(1)
+    alert(selectedColorData!.quantity)
+  }
 
   return (
     <div className="w-full grid grid-cols-8 pt-52">
@@ -17,7 +26,14 @@ const ProductPage = () => {
         <div className="col-span-4 w-full h-screen flex max-lg:flex-col max-lg:gap-y-7 max-lg:col-span-6 justify-center max-lg:items-center gap-x-12">
         <div className="w-1/2">
             {/* <ProductImages/> */}
-            <img src={ProductWhite} alt="ProductWhite" />
+            <img src={selectedColorImageUrl} alt="ProductWhite" />
+                    <div className="flex justify-start gap-x-6 mb-2">
+          {TrackMateColors.map((color) => (
+            <div className={`cursor-pointer ${color.color == selectedColor && 'border-2 border-[#e2c1ff]'}`} onClick={()=> setSelectedColor(color.color)}>
+              <img src={color.imageUrl} alt={color.color} className="w-12"/>
+            </div>
+          ))}
+        </div>
             <p className='text-lg min-[2260px]:text-2xl 2xl:text-xl max-sm:text-md max-[500px]:text-sm'>I'm a product description. I'm a great place to add more details about your product such as sizing, material, care instructions and cleaning instructions.</p>
         </div>
         <div className="w-1/2">
@@ -26,15 +42,15 @@ const ProductPage = () => {
         <p className='text-2xl mt-5'>$85.00</p>
         <p className='text-md mt-5'>Color</p>
         <div className="flex justify-start items-center gap-x-4 mt-3">
-          <div className="bg-white w-5 h-5 rounded-full border border-white"></div>
-          <div className="bg-blue-800 w-5 h-5 rounded-full border border-white"></div>
-          <div className="bg-purple-800 w-5 h-5 rounded-full border border-white"></div>
-          <div className="bg-black w-5 h-5 rounded-full border border-white"></div>
-          <div className="bg-[#1DB0A1] w-5 h-5 rounded-full border border-white"></div>
+          {
+            TrackMateColors.map((TrackMateColor) => (
+              <div className="bg-white w-5 h-5 rounded-full border border-white cursor-pointer" style={{backgroundColor: TrackMateColor.color}} onClick={() => setSelectedColor(TrackMateColor.color)}></div>
+            ))
+          }
         </div>
         <p className='text-md mt-5'>quantity</p>
-        <input type="number" defaultValue={1} min={1} max={5} className="w-24 h-12 bg-white mt-3 text-black pl-5 block"/>
-        <button className='bg-[#A238FF] text-white font-normal text-lg w-full px-12 py-2 rounded-3xl shadow-custom transition duration-300 hover:text-black hover:bg-white mt-10'>Add to cart</button>
+        <input type="number" onChange={(e) => setProductQuantity(Number(e.target.value))} value={productQuantity} defaultValue={1} min={1} max={5} className="w-24 h-12 bg-white mt-3 text-black pl-5 block"/>
+        <button className='bg-[#A238FF] text-white font-normal text-lg w-full px-12 py-2 rounded-3xl shadow-custom transition duration-300 hover:text-black hover:bg-white mt-10' onClick={() => addToCart()}>Add to cart</button>
         <div className="mt-6 flex flex-col justify-center items-center">
       <CollapsibleSection
         title="PRODUCT INFO"
---
+++ front-end/src/constants/constants.tsx
@@ -0,0 +1,35 @@
+import ProductWhite from "../Img/ProductImages/ProductWhite.webp"
+import ProductBlue from "../Img/ProductImages/ProductBlue.webp"
+import ProductPurple from "../Img/ProductImages/ProductPurple.webp"
+import ProductBlack from "../Img/ProductImages/ProductBlack.webp"
+import ProductTurquoise from "../Img/ProductImages/ProductTurquoise.webp"
+
+const TrackMateColors = [
+    {
+        color: "white",
+        imageUrl: ProductWhite,
+        quantity: 0
+    },
+    {
+        color: "blue",
+        imageUrl: ProductBlue,
+        quantity: 0
+    },
+    {
+        color: "purple",
+        imageUrl: ProductPurple,
+        quantity: 0
+    },
+    {
+        color: "black",
+        imageUrl: ProductBlack,
+        quantity: 0
+    },
+    {
+        color: "turquoise",
+        imageUrl: ProductTurquoise,
+        quantity: 0
+    }
+]
+
+export default TrackMateColors;
