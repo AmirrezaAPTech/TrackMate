95cdb7430ffc9d737d4c4ff344c193fab72beaf4
AmirrezaAP <AmirrezaAPTech@gmail.com>
2024-08-04 04:26:13 +0330

    Footer Hover

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -1,11 +1,5 @@
-import visa from "../Img/Footer/Visa.webp"
-import Diners from "../Img/Footer/Diners.webp"
-import Discover from "../Img/Footer/Discover.webp"
-import PayPal from "../Img/Footer/PayPal.webp"
-import Amex from "../Img/Footer/brand-amex_3x.webp"
-import Chinaunionpay from "../Img/Footer//brand-chinaunionpay_3x.webp"
-import Jcb from "../Img/Footer/brand-jcb_3x.webp"
-import Mastercard from "../Img/Footer/brand-mastercard_3x.webp"
+import { FooterList } from "../constants/constants"
+
 
 const Footer = () => {
   return (
@@ -14,16 +8,14 @@ const Footer = () => {
         <div className="w-full h-[1px] bg-white lg:hidden"></div>
             <div className="flex justify-center items-center gap-x-36 px-2">
                     <ul className="list-none flex max-lg:flex-col gap-y-8 justify-center items-center gap-x-12">
-                        <li>Instagram</li>
-                        <li>Facebook</li>
-                        <li>Twitter</li>
-                        <li>Tiktok</li>
-                        <li>Youtube</li>
+                        {
+                            FooterList.socialMedias.map(item =><a href={`https://${item}.com`}> <li className="font-normal transition-all duration-200">{item}</li></a>)
+                        }
                     </ul>
                     <ul className="list-none flex max-lg:flex-col max-lg:justify-between h-full lg:justify-center items-start gap-x-12 gap-y-8">
-                        <li>Terms & Conditions</li>
-                        <li>Shipping & Returns</li>
-                        <li>Privacy Policy</li>
+                        {
+                            FooterList.terms.map(item =><a href={`https://google.com`}> <li className="font-normal transition-all duration-200">{item}</li></a>)
+                        }
                     </ul>
             </div>
             <div className="w-[95%] h-[1px] bg-white max-lg:hidden"></div>
@@ -32,14 +24,9 @@ const Footer = () => {
                 <p>© 2035 by TrackMate. Created by <a href="https://github.com/AmirrezaAPTech" >AmirrezaAP</a></p>
             </div>
             <ul className="list-none flex justify-center flex-wrap gap-y-5 items-center gap-x-8">
-                        <li><img src={visa} alt="visa" /></li>
-                        <li><img src={Diners} alt="Diners" /></li>
-                        <li><img src={Discover} alt="Discover" /></li>
-                        <li><img src={PayPal} alt="PayPal" /></li>
-                        <li><img src={Amex} alt="Amex" /></li>
-                        <li><img src={Chinaunionpay} alt="Chinaunionpay" /></li>
-                        <li><img src={Jcb} alt="Jcb" /></li>
-                        <li><img src={Mastercard} alt="Mastercard" /></li>
+                        {
+                            FooterList.images.map(item => <li><img src={item} alt="images" /></li>)
+                        }
             </ul>
             </div>
         </div>
